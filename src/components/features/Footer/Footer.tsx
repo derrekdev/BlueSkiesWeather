f86ce9a2af1d7a84528c68222732c08b2de18042
Derrek Solitario <118510210+derrekdev@users.noreply.github.com>
2025-08-18 14:59:41 +0800

    added footer link to about us, updated footer layout

--- src/components/features/Footer/Footer.tsx
+++ src/components/features/Footer/Footer.tsx
@@ -1,3 +1,13 @@
+import "../../../styles/features/footer.scss";
+import LinkIcon from "../../ui/icons/LinkIcon";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <a href="/about">
+        <LinkIcon strokeWidth={2} width={20} height={20} />
+        About BlueSkiesWeather
+      </a>
+    </footer>
+  );
 }
