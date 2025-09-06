192d745d44d6f917c28d9a6876d2cc2f5968a08c
Derrek Solitario <118510210+derrekdev@users.noreply.github.com>
2025-09-06 23:37:53 +0800

    refactor weather api url and keys to .env file

--- src/services/apiWeather.ts
+++ src/services/apiWeather.ts
@@ -12,7 +12,11 @@ export const apiWeather = () => {
         : DEFAULT_LOCATION;
 
     return fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=32a55bd6f3a34377bb074242250808&q=${location}&days=1&aqi=no&alerts=no`
+      `${import.meta.env.VITE_WEATHER_API_URL}/${
+        import.meta.env.VITE_WEATHER_API_VERSION
+      }/forecast.json?key=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&q=${location}&days=1&aqi=no&alerts=no`
     ).then((response) => {
       return response.json();
     });
@@ -20,7 +24,11 @@ export const apiWeather = () => {
 
   const getLocationData = async (locationValue: string) => {
     return fetch(
-      `http://api.weatherapi.com/v1/search.json?key=32a55bd6f3a34377bb074242250808&q=${locationValue}`
+      `${import.meta.env.VITE_WEATHER_API_URL}/${
+        import.meta.env.VITE_WEATHER_API_VERSION
+      }/search.json?key=${
+        import.meta.env.VITE_WEATHER_API_KEY
+      }&q=${locationValue}`
     ).then((response) => {
       return response.json();
     });
